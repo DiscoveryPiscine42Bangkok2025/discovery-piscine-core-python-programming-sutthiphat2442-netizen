7ad025d8489b4bce8e0ba643174810244d7f7a6b
sutthiphat2442-netizen <sutthiphat2442@gmail.com>
2026-04-27 03:37:02 +0000

    hello

---
+++ cell06/ex01/upcase_it.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+def upcase_it(input_string):
+    """Returns the input string in uppercase."""
+    return input_string.upper()
+
+if __name__ == "__main__":
+    print(upcase_it("hello"))
